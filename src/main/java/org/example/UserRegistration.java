704426cf62ab2d37c26578ac29421136c0bbede8
mukesh007kls <mallikanti2000@gmail.com>
2023-03-16 12:46:25 +0530

    ADDED:Code to check last name

--- src/main/java/org/example/UserRegistration.java
+++ src/main/java/org/example/UserRegistration.java
@@ -4,10 +4,32 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public boolean userName(String firstName,String regex){
-        return Pattern.matches(regex,firstName);
+    private boolean userName(String name,String regex){
+        return Pattern.compile(regex).matcher(name).matches();
     }
+
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        UserRegistration userRegistration=new UserRegistration();
+        System.out.println("Enter the choice to choose which function to implement:-\n1.First Name\n2.Last Name");
+        int choice=sc.nextInt();
+        switch (choice){
+            case 1->userRegistration.firstName();
+            case 2->userRegistration.lastName();
+        }
+
+    }
+
+    private void lastName() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Last name:-");
+        String regex="[A-Z]{1}[a-z]{2,}";
+        String lastName=sc.nextLine();
+        UserRegistration userRegistration=new UserRegistration();
+        System.out.println(userRegistration.userName(lastName,regex));
+    }
+
+    private void firstName(){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the first name:-");
         String regex="[A-Z]{1}[a-z]{2,}";
